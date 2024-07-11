3b53ee43b7dfd4d2db31295c2fd29dcbb5f5f418
Amy <amy.lee.d2@gmail.com>
2024-07-11 11:29:46 +0900

    My Package 소스 수정

--- Sources/MyPackage/MyPackage.swift
+++ Sources/MyPackage/MyPackage.swift
@@ -1,2 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct MyPackage {
+    public private(set) var text = "Hello World"
+    
+    public init()
+    
+    public func test() {
+        print("Test Completed!!")
+    }
+}
