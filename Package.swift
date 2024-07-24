c9bf3fd4af929cc17ec0b70f917a0dbfa258be92
이지은 (Amy) <ezyeun@onestorecorp.com>
2024-07-24 15:45:38 +0900

    test

--- Package.swift
+++ Package.swift
@@ -4,21 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyPackage",
+    name: "MyPackageSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "MyPackage",
             targets: ["MyPackage"]),
+        .library(
+            name: "MyPackage2",
+            targets: ["MyPackage2"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyPackage"),
-        .testTarget(
-            name: "MyPackageTests",
-            dependencies: ["MyPackage"]),
+            name: "MyPackage"
+        ),
+        .target(
+            name: "MyPackage2",
+            dependencies: [
+                .target(name: "MyPackage")
+            ]
+        ),
     ],
     swiftLanguageVersions: [
         .v5
---
+++ Sources/MyPackage2/MyPackage2.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by 1000249 on 7/24/24.
+//
+
+import Foundation
+import MyPackage
+
+public struct MyPackage2 {
+    
+    public static let shared = MyPackage()
+    
+    public init() {
+        
+    }
+}
